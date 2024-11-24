b88be4cef0fb7a7f3482bcc3c5c5eda771aed54c
Sumith Shetty <sumithshetty014@gmail.com>
2024-11-24 14:28:33 +0530

    Installed WhiteNoise and Updated settings for production static file handling

--- livecollab/settings.py
+++ livecollab/settings.py
@@ -76,6 +76,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',   # WhiteNoiseMiddleware is added to handle static file serving in production environments.
 ]
 
 ROOT_URLCONF = 'livecollab.urls'
@@ -144,11 +145,31 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
+
+
+# The below code is used specifically for deployment to production environments.
+# 
+# - `STATIC_ROOT` is defined to collect all static files into a directory (`staticfiles`),
+#   which is required for serving static assets in production.
+# - The WhiteNoise storage backend (`STATICFILES_STORAGE`) is enabled to compress static files 
+#   (using gzip or Brotli) and append unique version identifiers to filenames. This ensures
+#   efficient file serving, reduces load times, and enables long-term caching.
+# - These settings are applied only in production (when DEBUG is False) to ensure compatibility
+#   with deployment requirements, as the development server handles static files differently.
+
+
+# This production code might break development mode, so we check whether we're in DEBUG mode
+if not DEBUG:    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
+    # and renames the files with unique names for each version to support long-term caching
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
